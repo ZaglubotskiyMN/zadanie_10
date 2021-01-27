bc925defe9bdabb7bea96c68b69568c95fc197ce
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-01-27 17:02:03 +0400

    Initial commit

---
+++ src/part1/lessons10/task1/HelloWorld.java
@@ -0,0 +1,48 @@
+package part1.lessons10.task1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        int[] array =new int[3];
+
+
+        try {
+            array[4]=15;
+            }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException"+" "+ "Текст ошибки:"+ " "+e.getMessage());
+        }
+
+        try{
+            throw null;
+        }catch (NullPointerException e){
+        System.out.println("NullPointerException"+" "+"Текст ошибки:"+ " "+e.getMessage());
+        }
+        try{
+            road bad=new road("Дорога на ремонте");
+            System.out.println(bad.getOfroad());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+}
+    static class road{
+        private String ofroad;
+        road(String ofroad){
+            this.setOfroad(ofroad);
+        }
+        public void road(String ofroad) throws Exception {
+         if (ofroad == "Дорога на ремонте") throw new Exception("Объезд");
+        }
+
+        public String getOfroad() {
+            return ofroad;
+        }
+
+        public void setOfroad(String ofroad) {
+            this.ofroad = ofroad;
+        }
+    }
+}
+
